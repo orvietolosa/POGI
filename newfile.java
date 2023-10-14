5e75addf4acbab53a9ccad648267e4f3654eb705
McDonald's <142709824+orvietolosa@users.noreply.github.com>
2023-10-13 19:43:38 -0700

    Add files via upload

---
+++ newfile.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+	
+  String result = "87";
+  int grade = 87;
+  grade=87;
+  result=(87>75)?"PASSED": "FAILED";
+  System.out.println("Your grade is 87");
+  System.out.println("The result is" + result);
+
+	}
+}
